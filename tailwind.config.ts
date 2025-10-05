90da635d679ef6119c98d9ad66221456293d1cab
Stanislav Kozachenko <stanislav.03@list.ru>
2025-10-05 19:13:30 +0300

    initial commit

---
+++ tailwind.config.ts
@@ -0,0 +1,50 @@
+import type { Config } from "tailwindcss";
+import { withUt } from "uploadthing/tw";
+
+const config = {
+    darkMode: "class",
+    content: [
+        "./pages/**/*.{ts,tsx}",
+        "./components/**/*.{ts,tsx}",
+        "./app/**/*.{ts,tsx}",
+        "./src/**/*.{ts,tsx}",
+    ],
+    theme: {
+        container: {
+            center: true,
+            padding: "2rem",
+            screens: {
+                "2xl": "1400px",
+            },
+        },
+        extend: {
+            borderRadius: {
+                lg: "var(--radius)",
+                md: "calc(var(--radius) - 2px)",
+                sm: "calc(var(--radius) - 4px)",
+            },
+            keyframes: {
+                "accordion-down": {
+                    from: { height: "0" },
+                    to: { height: "var(--radix-accordion-content-height)" },
+                },
+                "accordion-up": {
+                    from: { height: "var(--radix-accordion-content-height)" },
+                    to: { height: "0" },
+                },
+                "caret-blink": {
+                    "0%,70%,100%": { opacity: "1" },
+                    "20%,50%": { opacity: "0" },
+                },
+            },
+            animation: {
+                "accordion-down": "accordion-down 0.2s ease-out",
+                "accordion-up": "accordion-up 0.2s ease-out",
+                "caret-blink": "caret-blink 1.25s ease-out infinite",
+            },
+        },
+    },
+    plugins: [require("tailwindcss-animate")],
+} satisfies Config;
+
+export default withUt(config);
