8f42408cb08f46ea7e35aa80d8b306f3938e278b
Stanislav Kozachenko <stanislav.03@list.ru>
2026-03-06 21:37:45 +0300

    Added 'Categories' component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
 import {Button} from "@/components/ui/button";
+import {Container, Title, TopBar} from "@/components/shared";
 
 export default function Home() {
-  return (
-         <h1>
-             Главная
-         </h1>
-  );
+    return (
+        <>
+            <Container className="mt-10">
+                <Title text="Все пиццы" size="lg" className="font-extrabold" />
+
+            </Container>
+
+            <TopBar/>
+
+            <div style={{height: '3000px'}}></div>
+
+        </>
+    );
 }
