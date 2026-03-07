291b008964e2adc901d8dc167ffe9a0afb4a0703
Stanislav Kozachenko <stanislav.03@list.ru>
2026-03-07 14:05:30 +0300

    Added 'Filters, FilterCheckbox' components

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,22 @@
-import {Button} from "@/components/ui/button";
-import {Container, Title, TopBar} from "@/components/shared";
+import {Container, Title, TopBar, Filters, ProductsGroupList} from "@/components/shared";
 
 export default function Home() {
     return (
         <>
             <Container className="mt-10">
-                <Title text="Все пиццы" size="lg" className="font-extrabold" />
+                <Title text="Все пиццы" size="lg" className="font-extrabold"/>
 
             </Container>
 
             <TopBar/>
 
-            <div style={{height: '3000px'}}></div>
+            <Container className="mt-10 pb-14">
+                <div className="flex gap-[80px]">
+
+                    {/* Фильтрация */}
+                    <div className="w-[250px]">
+                        <Filters/>
+                    </div>
 
         </>
     );
