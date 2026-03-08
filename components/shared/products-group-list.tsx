fa09793757b788c003910bdd80151db38a2872c4
Stanislav Kozachenko <stanislav.03@list.ru>
2026-03-08 12:59:28 +0300

    The logic of product categories

--- components/shared/products-group-list.tsx
+++ components/shared/products-group-list.tsx
@@ -1,7 +1,11 @@
+'use client'
+
 import React from 'react';
+import {useIntersection} from 'react-use';
 import {ProductCard} from "@/components/shared/product-card";
 import {Title} from "@/components/shared/title";
-import {cn} from '@/lib/utils'
+import {cn} from '@/lib/utils';
+import {useCategoryStore} from "@/store/category";
 
 interface Props {
     title: string;
@@ -18,11 +22,25 @@ export const ProductsGroupList: React.FC<Props> = ({
                                                        categoryId,
                                                        className
                                                    }) => {
+
+    const setActiveCategoryId = useCategoryStore((state) => state.setActiveId);
+    const intersectionRef = React.useRef<HTMLDivElement>(null);
+
+    const intersection = useIntersection(intersectionRef as React.RefObject<HTMLElement>, {
+        threshold: 0.4,
+    });
+
+    React.useEffect(() => {
+        if(intersection?.isIntersecting) {
+            setActiveCategoryId(categoryId)
+        }
+    }, [categoryId, intersection?.isIntersecting, title])
+
     return (
-        <div className={className}>
+        <div className={className} id={title}>
             <Title text={title} size="lg" className="font-extrabold mb-5"/>
 
-            <div className={cn('grid grid-cols-3 gap-[50px]', listClassName)}>
+            <div ref={intersectionRef} className={cn('grid grid-cols-3 gap-[50px]', listClassName)}>
                 {
                     items.map((item, index) => (
                         <ProductCard
