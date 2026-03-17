18b705aa19ef2050ef5e3598f9f14657d0040070
Stanislav Kozachenko <stanislav.03@list.ru>
2026-03-17 21:09:17 +0300

    [Seed] Added prisma seed logic and configuration

---
+++ prisma/seed.ts
@@ -0,0 +1,107 @@
+
+import {hashSync} from 'bcrypt'
+import {categories, ingredients, products} from "@/prisma/constants";
+import {prisma} from "@/prisma/prisma-client";
+
+async function up() {
+    await prisma.user.createMany({
+        data: [
+            {
+                fullName: 'User Test',
+                email: 'user@test.ru',
+                password: hashSync('111111', 10),
+                verified: new Date(),
+                role: 'USER',
+            },
+            {
+                fullName: 'Admin Admin',
+                email: 'admin@test.ru',
+                password: hashSync('111111', 10),
+                verified: new Date(),
+                role: 'ADMIN',
+            },
+        ],
+    })
+
+    await prisma.category.createMany({
+        data: categories
+    });
+
+    await prisma.ingredient.createMany({
+        data: ingredients
+    })
+
+    await prisma.product.createMany({
+        data: products
+    });
+
+    const pizza1 = await prisma.product.create({
+        data: {
+            name: 'Пепперони фреш',
+            imageUrl:
+                'https://media.dodostatic.net/image/r:233x233/11EE7D61304FAF5A98A6958F2BB2D260.webp',
+            categoryId: 1,
+            ingredients: {
+                connect: ingredients.slice(0, 5),
+            },
+        },
+    });
+
+    const pizza2 = await prisma.product.create({
+        data: {
+            name: 'Сырная',
+            imageUrl:
+                'https://media.dodostatic.net/image/r:233x233/11EE7D610CF7E265B7C72BE5AE757CA7.webp',
+            categoryId: 1,
+            ingredients: {
+                connect: ingredients.slice(5, 10),
+            },
+        },
+    });
+
+    const pizza3 = await prisma.product.create({
+        data: {
+            name: 'Чоризо фреш',
+            imageUrl:
+                'https://media.dodostatic.net/image/r:584x584/11EE7D61706D472F9A5D71EB94149304.webp',
+            categoryId: 1,
+            ingredients: {
+                connect: ingredients.slice(10, 40),
+            },
+        },
+    });
+}
+
+async function down() {
+    const tables = [
+        'User',
+        'Product',
+        'Ingredient',
+        'Category',
+    ];
+
+    for (const table of tables) {
+        await prisma.$executeRawUnsafe(
+            `TRUNCATE TABLE "${table}" RESTART IDENTITY CASCADE;`
+        );
+    }
+}
+
+async function main() {
+    try {
+        await down();
+        await up();
+    } catch (e) {
+        console.error(e)
+    }
+}
+
+main()
+    .then(async () => {
+        await prisma.$disconnect();
+    })
+    .catch(async (e) => {
+        console.error(e);
+        await prisma.$disconnect();
+        process.exit(1);
+    });
