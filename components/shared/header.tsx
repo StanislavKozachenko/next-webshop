2d7ce5c022a8649d4331905945721ba2ef85aa3d
Stanislav Kozachenko <stanislav.03@list.ru>
2026-03-22 12:06:39 +0300

    Products search logic implemented

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import {cn} from "@/lib/utils";
-import {Container} from "@/components/shared";
+import {Container, SearchInput} from "@/components/shared";
 import Image from "next/image";
 import {Button} from "@/components/ui";
 import {ArrowRight, ShoppingCart, User} from "lucide-react";
+import Link from "next/link";
 
 interface Props {
     classname?: string;
@@ -14,13 +15,20 @@ export const Header: React.FC<Props> =({classname}) => {
         <header className={cn('border border-b',classname)}>
             <Container className='flex items-center justify-between py-8'>
                 {/*Left part*/}
-                <div className="flex items-center gap-4">
-                    <Image src="/logo.png" alt="Logo" width={35} height={35}/>
-                    <div>
-                        <h1 className="text-2xl uppercase font-black">Next Pizza</h1>
-                        <p className="text-sm text-gray-400 leading-3">вкусней уже некуда</p>
+                <Link href="/">
+                    <div className="flex items-center gap-4">
+                        <Image src="/logo.png" alt="Logo" width={35} height={35}/>
+                        <div>
+                            <h1 className="text-2xl uppercase font-black">Next Pizza</h1>
+                            <p className="text-sm text-gray-400 leading-3">вкусней уже некуда</p>
+                        </div>
                     </div>
+                </Link>
+
+                <div className="mx-10 flex-1">
+                    <SearchInput/>
                 </div>
+
                 {/*Right part*/}
                 <div className="flex items-cetner gap-3">
                     <Button variant="outline" className="flex items-center gap-1"><User size={16} />Войти</Button>
