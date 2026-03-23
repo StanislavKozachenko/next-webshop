e8bd0ada4116382e5a2f4163bfa03859cfb21858
Stanislav Kozachenko <stanislav.03@list.ru>
2026-03-23 18:14:00 +0300

    [Filters] Added ingredients service and refactored products search

--- services/products.ts
+++ services/products.ts
@@ -3,5 +3,6 @@ import {Product} from "@prisma/client";
 import {ApiRoutes} from "@/services/constants";
 
 export const search = async (query: string): Promise<Product[]> => {
-    return (await axiosInstance.get<Product[]>(ApiRoutes.SEARCH_PRODUCTS, {params: {query}})).data;
+    const {data} = await axiosInstance.get<Product[]>(ApiRoutes.SEARCH_PRODUCTS, {params: {query}})
+    return data;
 }
