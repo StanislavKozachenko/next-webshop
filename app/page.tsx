5c2a9a50273c6ac793e47401ddeaf505dcc3a145
Stanislav Kozachenko <stanislav.03@list.ru>
2026-04-11 11:37:43 +0300

    [Products] Display categories from DB

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default async function Home() {
 
             </Container>
 
-            <TopBar/>
+            <TopBar categories={categories.filter((category) => category.products.length > 0)}/>
 
             <Container className="mt-10 pb-14">
                 <div className="flex gap-[80px]">
